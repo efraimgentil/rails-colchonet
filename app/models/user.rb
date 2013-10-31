fb11d0ec38792b3e9f0104da0ee0231170adc142
EfraimGentil <efraim.gentil@gmail.com>
2013-10-30 23:58:34 -0300

    Adiçãode login, e scopos de consulta em User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
+# encoding: utf-8
 class User < ActiveRecord::Base
+	# default_scope where('confirmed_at IS NOT NULL')
+	scope :confirmed, where('confirmed_at IS NOT NULL')
+	scope :most_recent, order('created_at DESC')
+	scope :form_sampa, where(:location => 'São Paulo')
+	scope :from, ->(location) { where(:location => location ) }
+	#Exemplo de uso User.from("Cidade")  ou #Exemplo de uso User.from("Cidade").limit 1
 
 	validates_presence_of :email, :full_name, :location
 	#:password
@@ -10,6 +17,12 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	before_create :generate_token
+
+  def self.authenticate( email , password)
+  	
+  	confirmed.find_by_email(email).try(:authenticate , password)
+  	
+  end
 
 	def generate_token
 		self.confirmation_token = SecureRandom.urlsafe_base64
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Colchonet::Application.routes.draw do
     resources :rooms
     resources :users
     resource  :confirmation, :only => [:show]
+    resource :user_sessions, :only => [:create, :new , :destroy]
   end
 
 
