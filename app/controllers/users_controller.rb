682382bb173b74493504109efbfaba9e253a958c
EfraimGentil <efraim.gentil@gmail.com>
2013-11-03 19:17:00 -0300

    Adição de filtros nos controllers de room, user, user_session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+    before_filter :require_no_authentication, :only => [ :new , :create]
+    before_filter :can_change, :only => [:edit, :update]
 
     def edit
         @user = User.find params[:id]
@@ -36,6 +37,16 @@ class UsersController < ApplicationController
     private 
         def allowed_params
             params.require(:user).permit(:full_name,:email, :location ,:password,:password_confirmation, :bio)
+        end
+
+        def can_change
+            unless user_signed_in? && current_user == user
+                redirect_to user_path(params[:id])
+            end
+        end
+
+        def user
+            @user ||= User.find(params[:id])            
         end
 
 end
